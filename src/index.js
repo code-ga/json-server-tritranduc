020b6e76a5f187108a29f31529eb33bbba517a1a
tritranduc <72681678+tritranduc@users.noreply.github.com>
2021-06-14 20:57:11 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 var router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
+var port = process.env.PORT || 8080
 
 server.use(middlewares);
 server.use(router);
-server.listen(8080, () => {
+server.listen(port, () => {
   console.log("JSON Server is running");
 });
